65468b2435b93cc4ad9118341f6d389aa6a3b0aa
Animesh Trivedi <animesh.trivedi@gmail.com>
2017-11-24 11:19:21 +0100

    new setup and API for tests

--- src/main/scala/com/github/animeshtrivedi/FileBench/AbstractTest.scala
+++ src/main/scala/com/github/animeshtrivedi/FileBench/AbstractTest.scala
@@ -5,7 +5,26 @@ package com.github.animeshtrivedi.FileBench
   */
 case class TestResult(rows:Long, bytes:Long, runtimeNanoSec:Long)
 
-trait AbstractTest extends Runnable with Serializable {
-  def init(fileName:String, expectedBytes:Long)
-  def getResults():TestResult
+abstract class AbstractTest extends Runnable with Serializable {
+  protected var _sum:Long = 0L
+  protected var _validDecimal:Long = 0L
+  protected var _validInt:Long = 0L
+  protected var _validLong:Long = 0L
+  protected var _validDouble:Long = 0L
+
+  protected var runTimeInNanoSecs = 0L
+  protected var readBytes = 0L
+  protected var totalRows = 0L
+
+
+  protected def printStats(){
+    println("sum: " + this._sum +
+      " | ints: " + this._validInt +
+      " longs: " + this._validLong +
+      " doubles:" + this._validDouble +
+      " decimals: " + this._validDecimal)
+  }
+  final def getResults:TestResult = TestResult(this.totalRows, this.readBytes, this.runTimeInNanoSecs)
+
+  abstract def init(fileName:String, expectedBytes:Long)
 }
