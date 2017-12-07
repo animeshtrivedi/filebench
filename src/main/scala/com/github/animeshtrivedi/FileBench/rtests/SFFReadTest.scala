fdb6480db803101a06dae2d31d34e5cc612d1217
Animesh Trivedi <animesh.trivedi@gmail.com>
2017-12-07 14:59:48 +0100

    working example

--- src/main/scala/com/github/animeshtrivedi/FileBench/rtests/SFFReadTest.scala
+++ src/main/scala/com/github/animeshtrivedi/FileBench/rtests/SFFReadTest.scala
@@ -1,8 +1,8 @@
 package com.github.animeshtrivedi.FileBench.rtests
 
 import com.github.animeshtrivedi.FileBench.{AbstractTest, TestObjectFactory}
-import org.apache.spark.sql.simplefileformat.SimpleFileFormat
-import org.apache.spark.sql.simplefileformat.priv.SFFRow
+import org.apache.spark.sql.simplefileformat.{SFFRowIterator, SimpleFileFormat}
+import org.apache.spark.sql.simplefileformat.priv.SFFROW
 import org.apache.spark.sql.types._
 
 /**
@@ -10,20 +10,20 @@ import org.apache.spark.sql.types._
   */
 class SFFReadTest extends  AbstractTest {
   private[this] val sff = new SimpleFileFormat
-  private[this] var itr:Iterator[SFFRow] = _
+  private[this] var itr:SFFRowIterator = _
   private[this] var schema:StructType = _
-  private[this] var fastReaders:Array[SFFRow => Unit] = _
+  private[this] var fastReaders:Array[SFFROW => Unit] = _
   private[this] var schemaArray:Array[DataType] = _
   private[this] var numCols:Int = _
 
-  private[this] val extractLongFunc:(Int, SFFRow)=>Unit = (ordinal:Int, row:SFFRow) => {
+  private[this] val extractLongFunc:(Int, SFFROW)=>Unit = (ordinal:Int, row:SFFROW) => {
     if(!row.isNullAt(ordinal)){
       this._sum+=row.getLong(ordinal)
       this._validLong+=1
     }
   }
 
-  private[this] val extractIntFunc:(Int, SFFRow)=>Unit = (ordinal:Int, row:SFFRow) => {
+  private[this] val extractIntFunc:(Int, SFFROW)=>Unit = (ordinal:Int, row:SFFROW) => {
     if(!row.isNullAt(ordinal)){
       this._sum+=row.getInt(ordinal)
       this._validInt+=1
@@ -33,20 +33,20 @@ class SFFReadTest extends  AbstractTest {
   final override def init(fileName: String, expectedBytes: Long): Unit = {
     this.readBytes = expectedBytes
     this.itr = sff.buildRowIteratorX(fileName)
-    this.schema = sff.getSchemaFromDatafile(fileName)
+    this.schema = this.itr.getSFFMetadata.getSchema()
     this.schemaArray = this.schema.fields.map(fx => fx.dataType)
     this.numCols = this.schema.fields.length
-    //    this.fastReaders = new Array[SFFRow => Unit](this.schema.size)
+    //    this.fastReaders = new Array[SFFROW => Unit](this.schema.size)
     //    for(i <- this.fastReaders.indices){
     //      this.schema.fields(i).dataType match {
-    //        case LongType => this.fastReaders(i) = extractLongFunc(i, _:SFFRow)
-    //        case IntegerType => this.fastReaders(i) = extractIntFunc(i, _:SFFRow)
+    //        case LongType => this.fastReaders(i) = extractLongFunc(i, _:SFFROW)
+    //        case IntegerType => this.fastReaders(i) = extractIntFunc(i, _:SFFROW)
     //        case _ => throw new Exception
     //      }
     //    }
   }
 
-  private[this] def consumeSFFRowX2(row:SFFRow):Unit= {
+  private[this] def consumeSFFROWX2(row:SFFROW):Unit= {
     // TODO: to see if scala.Function are inlined or not?
     var i = 0
     while (i < numCols){
@@ -55,7 +55,7 @@ class SFFReadTest extends  AbstractTest {
     }
   }
 
-  private[this] def consumeSFFRowX4Debug(row:SFFRow):Unit= {
+  private[this] def consumeSFFROWX4Debug(row:SFFROW):Unit= {
     val sb = new StringBuilder
     var i = 0
     while (i < numCols){
@@ -84,14 +84,24 @@ class SFFReadTest extends  AbstractTest {
         }else {
           sb.append(" null ")
         }
+        case BinaryType => if (!row.isNullAt(i)) {
+          this._validBinary += 1
+          val size = row.getColumnSizeinBytes(i)
+          this._sum += size
+          this._validBinarySize+=size
+          sb.append("Binary(" + size + " bytes)")
+        } else {
+          sb.append("Binary(" + null + " bytes)")
+        }
         case _ => throw new Exception(" not implemented type " + this.schemaArray(i))
       }
       i+=1
+      sb.append(" | ")
     }
     println(sb.mkString)
   }
 
-  private[this] def consumeSFFRowX4(row:SFFRow):Unit= {
+  private[this] def consumeSFFROWX4(row:SFFROW):Unit= {
     var i = 0
     while (i < numCols) {
       this.schemaArray(i) match {
@@ -107,6 +117,12 @@ class SFFReadTest extends  AbstractTest {
           this._validDouble += 1
           this._sum += row.getDouble(i).toLong
         }
+        case BinaryType => if (!row.isNullAt(i)) {
+          this._validBinary += 1
+          val size = row.getColumnSizeinBytes(i)
+          this._sum+=size
+          this._validBinarySize+=size
+        }
         case _ => throw new Exception(" not implemented type " + this.schemaArray(i))
       }
       i+=1
@@ -118,7 +134,7 @@ class SFFReadTest extends  AbstractTest {
     val s1 = System.nanoTime()
     while(itr.hasNext){
       val row = itr.next()
-      consumeSFFRowX4(row)
+      consumeSFFROWX4(row)
       totalRows+=1
     }
     this.runTimeInNanoSecs = System.nanoTime() - s1
