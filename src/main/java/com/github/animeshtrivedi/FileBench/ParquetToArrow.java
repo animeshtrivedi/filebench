d447f4fe3706e363dd36c823a33e30d284d76a3a
Animesh Trivedi <animesh.trivedi@gmail.com>
2017-12-19 17:55:18 +0100

    a working parquet -> arrow writer

--- src/main/java/com/github/animeshtrivedi/FileBench/ParquetToArrow.java
+++ src/main/java/com/github/animeshtrivedi/FileBench/ParquetToArrow.java
@@ -3,9 +3,7 @@ package com.github.animeshtrivedi.FileBench;
 import com.github.animeshtrivedi.FileBench.helper.DumpGroupConverter;
 import com.google.common.collect.ImmutableList;
 import org.apache.arrow.memory.RootAllocator;
-import org.apache.arrow.vector.FieldVector;
-import org.apache.arrow.vector.NullableIntVector;
-import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.*;
 import org.apache.arrow.vector.dictionary.DictionaryProvider;
 import org.apache.arrow.vector.file.ArrowFileWriter;
 import org.apache.arrow.vector.types.FloatingPointPrecision;
@@ -26,6 +24,7 @@ import org.apache.parquet.format.converter.ParquetMetadataConverter;
 import org.apache.parquet.hadoop.ParquetFileReader;
 import org.apache.parquet.hadoop.metadata.FileMetaData;
 import org.apache.parquet.hadoop.metadata.ParquetMetadata;
+import org.apache.parquet.io.api.Binary;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.OriginalType;
 import org.apache.parquet.schema.PrimitiveType;
@@ -152,9 +151,9 @@ public class ParquetToArrow {
                 ColumnDescriptor col = colDesc.get(i);
                 switch(col.getType()) {
                     case INT32: writeIntColumn(colReader, col, fieldVectors.get(i)); break;
-                    case INT64: break;
-                    case DOUBLE: break;
-                    case BINARY: break;
+                    case INT64: writeLongColumn(colReader, col, fieldVectors.get(i)); break;
+                    case DOUBLE: writeDoubleColumn(colReader, col, fieldVectors.get(i)); break;
+                    case BINARY: writeBinaryColumn(colReader, col, fieldVectors.get(i)); break;
                     default : throw new Exception(" NYI " + col.getType());
                 }
                 i+=1;
@@ -187,6 +186,95 @@ public class ParquetToArrow {
                 mutator.setIndexDefined(i);
                 // there is setSafe too - what does that mean? TODO:
                 mutator.set(i, 1, x);
+            } else {
+                mutator.setNull(i);
+            }
+            creader.consume();
+        }
+        mutator.setValueCount((int) rows);
+        return rows;
+    }
+
+    private long writeLongColumn(ColumnReadStoreImpl crstore,
+                                org.apache.parquet.column.ColumnDescriptor column,
+                                FieldVector fieldVector) throws Exception {
+        int dmax = column.getMaxDefinitionLevel();
+        ColumnReader creader = crstore.getColumnReader(column);
+        long rows = creader.getTotalValueCount();
+        if(rows > Integer.MAX_VALUE)
+            throw new Exception(" More than Integer.MAX_VALUE is not supported " + rows);
+
+        NullableBigIntVector bigIntVector = (NullableBigIntVector) fieldVector;
+        NullableBigIntVector.Mutator mutator = bigIntVector.getMutator();
+        bigIntVector.setInitialCapacity((int) rows);
+        bigIntVector.allocateNew();
+
+        for(int i = 0; i < (int) rows; i++) {
+            if(creader.getCurrentDefinitionLevel() == dmax){
+                long x = creader.getLong();
+                // do something with x
+                mutator.setIndexDefined(i);
+                // there is setSafe too - what does that mean? TODO:
+                mutator.set(i, 1, x);
+            } else {
+                mutator.setNull(i);
+            }
+            creader.consume();
+        }
+        mutator.setValueCount((int) rows);
+        return rows;
+    }
+
+    private long writeDoubleColumn(ColumnReadStoreImpl crstore,
+                                 org.apache.parquet.column.ColumnDescriptor column,
+                                 FieldVector fieldVector) throws Exception {
+        int dmax = column.getMaxDefinitionLevel();
+        ColumnReader creader = crstore.getColumnReader(column);
+        long rows = creader.getTotalValueCount();
+        if(rows > Integer.MAX_VALUE)
+            throw new Exception(" More than Integer.MAX_VALUE is not supported " + rows);
+
+        NullableFloat8Vector float8Vector  = (NullableFloat8Vector ) fieldVector;
+        NullableFloat8Vector.Mutator mutator = float8Vector.getMutator();
+        float8Vector.setInitialCapacity((int) rows);
+        float8Vector.allocateNew();
+
+        for(int i = 0; i < (int) rows; i++) {
+            if(creader.getCurrentDefinitionLevel() == dmax){
+                double x = creader.getDouble();
+                // do something with x
+                mutator.setIndexDefined(i);
+                // there is setSafe too - what does that mean? TODO:
+                mutator.set(i, 1, x);
+            } else {
+                mutator.setNull(i);
+            }
+            creader.consume();
+        }
+        mutator.setValueCount((int) rows);
+        return rows;
+    }
+
+    private long writeBinaryColumn(ColumnReadStoreImpl crstore,
+                                   org.apache.parquet.column.ColumnDescriptor column,
+                                   FieldVector fieldVector) throws Exception {
+        int dmax = column.getMaxDefinitionLevel();
+        ColumnReader creader = crstore.getColumnReader(column);
+        long rows = creader.getTotalValueCount();
+        if(rows > Integer.MAX_VALUE)
+            throw new Exception(" More than Integer.MAX_VALUE is not supported " + rows);
+
+        NullableVarBinaryVector varBinaryVector  = (NullableVarBinaryVector) fieldVector;
+        NullableVarBinaryVector.Mutator mutator = varBinaryVector.getMutator();
+        varBinaryVector.setInitialCapacity((int) rows);
+        varBinaryVector.allocateNew();
+        for(int i = 0; i < (int) rows; i++) {
+            if(creader.getCurrentDefinitionLevel() == dmax){
+                byte[] data = creader.getBinary().getBytes();
+                // do something with x
+                mutator.setIndexDefined(i);
+                mutator.setValueLengthSafe(i, data.length);
+                mutator.set(i, data);
             } else {
                 mutator.setNull(i);
             }
